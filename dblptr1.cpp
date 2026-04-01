5b6758491fe467c080d5e8e995d7b5a5c50e403f
pooja mishra <poojagudiya851@gmail.com>
2026-04-01 07:27:49 +0530

    Add files via upload

---
+++ dblptr1.cpp
@@ -0,0 +1,36 @@
+
+#include<iostream>
+using namespace std;
+
+
+int main() {
+
+    int i = 5;
+    int* p = &i;
+    int** p2 = &p;
+
+    cout<< endl << endl <<" Sab sahi chal rha h " << endl << endl ;
+
+    cout << i << endl;
+    cout << *p << endl;
+    cout << **p2 << endl;
+
+    cout << &i << endl;
+    cout << p << endl;
+    cout << *p2 << endl;
+
+    cout << &p << endl;
+    cout << p2 << endl;
+
+    
+    
+
+
+
+
+        
+
+
+
+    return 0;
+}
