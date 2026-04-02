d8b79ae97b756ee7abd0a71bd24bc3d650c37d02
pooja mishra <poojagudiya851@gmail.com>
2026-04-02 21:47:27 +0530

    Add files via upload

---
+++ p03.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+    // int arr[10] ={23, 122, 41 , 67};
+    // cout << "address of first memory block is " << arr << endl;
+    // cout << arr[0] << endl;
+    // cout << "address of first memory block is" << &arr[0] << endl;
+    // cout << "4th" << *arr << endl;
+    // cout << "5th" << *arr+1 << endl;
+    // cout << "6th" << *(arr+1) << endl;
+    // cout << "7th" << *(arr) + 1 << endl;
+    // cout << "8th" << arr[2] << endl;
+    // cout << "9th" << *(arr+2) << endl;
+    // // imp
+    // int i = 3;
+    // cout << i[arr] << endl;
+
+//  int temp[10] = {1,2};
+//  cout << sizeof(temp) << endl;
+//  cout << "1st" << sizeof(*temp) << endl;
+//  cout << "2nd" << sizeof(&temp) << endl;
+//  int *ptr = &temp[0];
+//  cout << sizeof(ptr) << endl;
+//  cout << sizeof(*ptr) << endl;
+//  cout << sizeof(&ptr) << endl;
+int arr[10];
+// ERROR (IMP)
+// arr = arr+1;
+int *ptr = &arr[0];
+cout << ptr << endl;
+ ptr = ptr+1;
+ cout << ptr << endl;
+ return 0;
+}
