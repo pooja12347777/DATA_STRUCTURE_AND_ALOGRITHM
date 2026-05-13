02f943077d88ee60479a0d1d8cb10988c516680f
pooja mishra <poojagudiya851@gmail.com>
2026-05-13 19:04:35 +0530

    Add files via upload

---
+++ opps_c++/encap.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class Student{
+    private:
+    string name;
+    int age;
+    public:
+    int getage(){
+        return this->age;
+
+    }
+};
+int main(){
+    Student first;
+    cout<<"good";
+    return 0;
+}
+
+
