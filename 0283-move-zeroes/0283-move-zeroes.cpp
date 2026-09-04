3ae3bccc1bbee09f6e83d58b7ad0f8c6590f107b
pooja mishra <poojagudiya851@gmail.com>
2026-09-04 09:10:00 +0530

    Time: 0 ms (100%), Space: 23.8 MB (82.73%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n = nums.size();
+        int position=0;
+         
+       
+        for(int i =0;i<n;i++){
+            if(nums[i]!= 0){
+                swap(nums[i],  nums[position]);
+                position ++;
+
+               
+            }
+            
+            
+        }
+
+        
+    }
+};
