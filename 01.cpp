151d4255d9a6a9c52b8983fe2bc92cc34d9a4bdd
pooja mishra <poojagudiya851@gmail.com>
2026-09-15 23:25:44 +0530

    Add files via upload

--- 01.cpp
+++ 01.cpp
@@ -1,119 +1,34 @@
 #include<iostream>
-#include<stack>
 using namespace std;
-
-class Stack {
-    //properties
+class node{
     public:
-        int *arr;
-        int top;
-        int size;
-
-    // behaviour
-    Stack(int size) {
-        this -> size = size;
-        arr = new int[size];
-        top = -1;
+    int data;
+    node*left;
+    node*right;
+    node(int d){
+        this->data = d;
+        this->left = NULL;
+        this->right = NULL;
     }
-
-    void push( int element) {
-        if(size - top > 1) {
-            top++;
-            arr[top] = element;
-        }
-        else{
-            cout << "Stack OverFlow" << endl;
-        }
-    }
-
-    void pop() {
-        if(top >=0 ) {
-            top--;
-        }
-        else{
-            cout << "Stack UnderFlow" << endl;
-        }
-    }
-
-    int peek() {
-        if(top >=0 )
-            return arr[top];
-        else
-        {
-            cout << "Stack is Empty" << endl;
-            return -1;
-        }
-    }
-
-    bool isEmpty() {
-        if( top == -1) {
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-
 };
-
-
-int main() {
-
-
-    Stack st(5);
-
-    st.push(22);
-    st.push(43);
-    st.push(44);
-    st.push(22);
-    st.push(43);
-    st.push(44);
-
-    cout << st.peek() << endl;
-
-    st.pop();
-
-    cout << st.peek() << endl;
-
-    st.pop();
-
-    cout << st.peek() << endl;
-
-    st.pop();
-
-    cout << st.peek() << endl;
-
-    if(st.isEmpty()) {
-        cout << "Stack is Empty  " << endl;
+node* buildtree(node*root){
+    cout << "enter data" << endl;
+    int data;
+    cin >> data;
+    root = new node(data);
+    if(data == -1){
+        return NULL;
     }
-    else{
-        cout << "Stack is not Empty  " << endl;
-    }
+    cout << "enter data for inserting in left of " << data << endl;
+    root->left = buildtree(root->left);
+    cout << "enter data for inserting in right of " << data<< endl;
+    root->right = buildtree(root->right);
+    return root;
 
-    
-    /*
-    //creation of stack
-    stack<int> s;
-
-    //push operation
-    s.push(2);
-    s.push(3);
-
-    //pop
-    s.pop();
-
-    cout << "Printing top element " << s.top() << endl;
-
-    if(s.empty()){
-        cout << "Stack is empty " << endl;
-    }
-    else{
-        cout << "stack is not empty " << endl;
-    }
-
-    cout << "size of stack is " << s.size() << endl;
-
-    */
-
+}
+int main(){
+    node* root = NULL;
+    root = buildtree(root);
+    cout << "printing the level order tracersal output" << endl;
     return 0;
 }
