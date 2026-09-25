11068b2455986b64a89d133375596fb1f4c9f095
pooja mishra <poojagudiya851@gmail.com>
2026-09-25 19:38:48 +0530

    Add files via upload

---
+++ upperbound.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+  public:
+    int upperBound(vector<int>& arr, int target) {
+        // code here
+        int n = arr.size();
+        int s =0;
+        int  e = n-1;
+        int ans = n;
+          int mid = s+(e-s)/2;
+          while(s<=e){
+              if(target == arr[mid]){
+                  
+                  s = mid+1;
+              }
+              else if(target>arr[mid]){
+                 
+                  s = mid+1;
+              }
+              else if (target<arr[mid]){
+                   ans = mid;
+                  e = mid-1;
+              }
+              mid = s + (e-s)/2;
+          }
+          return ans;
+    }
+};
+int main() {
+    Solution obj;
+    vector<int> arr = {2, 4, 3, 8};
+    int target = 5;
+
+    cout << obj.upperBound(arr, target) << endl;  // Expected: 2
+    return 0;
+}
